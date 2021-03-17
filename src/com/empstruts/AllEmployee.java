8d84c597df0a3b90ac2861248751e350aabe9aef
Mustufa Qadri <80328402+mustufa-sibisoft@users.noreply.github.com>
2021-03-17 15:58:13 +0500

    code structure

--- src/com/empstruts/AllEmployee.java
+++ src/com/empstruts/AllEmployee.java
@@ -4,19 +4,10 @@ import org.apache.struts.action.ActionForm;
 
 public class AllEmployee extends ActionForm {
 	private static final long serialVersionUID = 1L;
-	private String hello = null;
 
 	public AllEmployee() {
 		super();
 		EmployeeManager.loadRecord();
-		//hello = "Hello All Employees";
 	}
 
-	public String getHello() {
-		return hello;
-	}
-
-	public void setHello(String hello) {
-		this.hello = hello;
-	}
 }
--- src/com/empstruts/DeleteRecord.java
+++ src/com/empstruts/DeleteRecord.java
@@ -4,19 +4,9 @@ import org.apache.struts.action.ActionForm;
 
 public class DeleteRecord extends ActionForm {
 	private static final long serialVersionUID = 1L;
-	private String hello = null;
 
 	public DeleteRecord() {
 		super();
-		hello = "Delete All Employees";
-	}
-
-	public String getHello() {
-		return hello;
-	}
-
-	public void setHello(String hello) {
-		this.hello = hello;
 	}
 
 	public static void deleteEmployee(int id) {
